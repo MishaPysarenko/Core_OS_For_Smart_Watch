9be11049637f4363fd4cbecd615db58a2c362f3d
MishaPysarenko <pmsmihsa@gmail.com>
2026-03-09 20:01:14 +0200

    new file for task scheduler

---
+++ Core/Inc/taskScheduler.h
@@ -0,0 +1,13 @@
+/*
+ * taskScheduler.h
+ *
+ *  Created on: Mar 9, 2026
+ *      Author: Mike
+ */
+
+#ifndef INC_TASKSCHEDULER_H_
+#define INC_TASKSCHEDULER_H_
+
+
+
+#endif /* INC_TASKSCHEDULER_H_ */
